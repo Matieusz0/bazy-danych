416f11d39c7e7a3e4bbbdf623f8e77081b86625e
Matieusz <matieusz2137@gmail.com>
2025-01-27 10:05:43 +0100

    Create zadanie2.sql

---
+++ zadanie2.sql
@@ -0,0 +1,20 @@
+create data if not EXISTS zadanie2 DEFAULT CHARACTER set utf8mb4 COLLATE uft8mb4_unicode_ci;
+use zadanie2;
+
+create table samochod
+(
+    id int not null primary key AUTO_INCREMENT,
+    vin varchar(50) not null UNIQUE,
+    numer_rejestracyjny varchar(50) not null UNIQUE
+);
+create table wazenie
+(
+  samochod_id int not null,
+  masa_pojazdu bigint not null,
+  data_wazenia timestamp not null DEFAULT CURRENT_TIMESTAMP on UPDATE CURRENT_TIMESTAMP
+);
+
+alter table wazenie add CONSTRAINT FOREIGN key
+fk_samochod_wazenie_samochod_id(samochod_id)
+REFERENCES samochod(id)
+on UPDATE RESTRICT on delete RESTRICT;
