30f972b9a18c0bbe78ec5dc861b128c528a2391d
Matieusz <matieusz2137@gmail.com>
2025-01-28 13:46:11 +0100

    Update zadanie3.sql

--- zadanie3.sql
+++ zadanie3.sql
@@ -7,14 +7,14 @@ create table stacje(
     kod int not null UNIQUE,
     szerokosc_geograficzna decimal(8,6) not null,
     dlugosc_geograficzna decimal(8,6) not null,
-    kraj_instalacji varchar(50) not null,
+    kraj_id int not null,
     data_instalacji timestamp not null,
     data_kolejnego_przegladu timestamp not null DEFAULT CURRENT_TIMESTAMP
 );
 
-create table opady(
-    id int not null PRIMARY KEY AUTO_INCREMENT,
-    rodzaj varchar(50) not null
+create table kraj(
+    id int not null primary key AUTO_INCREMENT,
+    kraj varchar(50) not null
 );
 
 create table rodzajpomiaru(
@@ -26,8 +26,7 @@ create table pomiary(
   	Data_wykonania timestamp not null DEFAULT CURRENT_TIMESTAMP,
     stacja_id int not null,
     rodzaj_pomiaru_id int not null,
-    wartosc decimal(4,3) not null,
-    rodzaj_opadow_id int
+    wartosc decimal(4,3) not null
 );
 
 alter table pomiary add CONSTRAINT FOREIGN key
@@ -36,11 +35,11 @@ REFERENCES stacje(id)
 on UPDATE RESTRICT on delete RESTRICT;
 
 alter table pomiary add CONSTRAINT FOREIGN key
-fk_opady_pomiary_rodzaj_opadow_id(rodzaj_opadow_id)
-REFERENCES opady(id)
-on UPDATE RESTRICT on delete RESTRICT;
-
-alter table pomiary add CONSTRAINT FOREIGN key
 fk_rodzajpomiaru_pomiary_rodzaj_pomiaru_id(rodzaj_pomiaru_id)
 REFERENCES rodzajpomiaru(id)
+on UPDATE RESTRICT on delete RESTRICT;
+
+alter table stacje add CONSTRAINT FOREIGN key
+fk_kraj_stacje_kraj_id(kraj_id)
+REFERENCES kraj(id)
 on UPDATE RESTRICT on delete RESTRICT;
